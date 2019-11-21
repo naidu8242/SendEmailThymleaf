999624f397cc68995add0aaad6863e12a1b053ca
naidu8242 <plakshunnaidu@gmail.com>
2019-11-21 10:03:41 +0530

    sending email using spring boot thymleaf

---
+++ mail-template-thymeleaf/src/main/java/com/memorynotfound/mail/EmailService.java
@@ -0,0 +1,42 @@
+package com.memorynotfound.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    @Autowired
+    private SpringTemplateEngine templateEngine;
+
+
+    public void sendSimpleMessage(Mail mail) throws MessagingException, IOException {
+        MimeMessage message = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message,  MimeMessageHelper.MULTIPART_MODE_MIXED_RELATED, StandardCharsets.UTF_8.name());
+
+        Context context = new Context();
+        context.setVariables(mail.getModel());
+        String html = templateEngine.process("registationEmail", context);
+
+        helper.setTo(mail.getTo());
+        helper.setText(html, true);
+        helper.setSubject(mail.getSubject());
+        helper.setFrom(mail.getFrom());
+
+        emailSender.send(message);
+    }
+
+}
